09df3b4ec7a69ac7f0d080ff37b0890d55f2f2a7
SharathChandraS <47963190+SharathChandraS@users.noreply.github.com>
2019-02-25 12:58:02 +0530

    Error debuged and added input stream.

--- Javasample.java
+++ Javasample.java
@@ -1,8 +1,8 @@
 public class  Javasample{
 	public static void main(String[] args) {
-		int N=24;
-		System.out.println(N%2);
-		if(N/2==0){
+		Scanner scanner=new Scanner(Sysem.in);
+		int N=scanner.nextInt();
+		if(N%2==0){
 	            if(N<=20){
 	                 if(N>=2 && N<5){
 	                System.out.println("Not Weird");
