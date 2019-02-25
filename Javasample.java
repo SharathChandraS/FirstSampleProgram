faac8f48e75006bc2099966ef06fd9c55edc461b
SharathChandraS <47963190+SharathChandraS@users.noreply.github.com>
2019-02-25 12:55:47 +0530

    1. Print weird for odd numbers, (6-20) and 2.Print Not Weird for (2-5) numbers ,above 20 & even number .

---
+++ Javasample.java
@@ -0,0 +1,21 @@
+public class  Javasample{
+	public static void main(String[] args) {
+		int N=24;
+		System.out.println(N%2);
+		if(N/2==0){
+	            if(N<=20){
+	                 if(N>=2 && N<5){
+	                System.out.println("Not Weird");
+	            }else{
+	                System.out.println("Weird");
+	            }
+	                
+	            }else{
+	                
+	               System.out.println("Not Weird");
+	            }
+	        }else{
+	            System.out.println("Weird");
+	        }
+}
+}
